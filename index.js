c3bf24b27cbc52b1f639f6399cb4a9c198c11d69
nathbrl <nathalie.barruel@outlook.fr>
2021-04-22 16:00:10 +0200

    first commit
    html, css index and js for the index page

---
+++ index.js
@@ -0,0 +1,25 @@
+function openArticleOne() {
+    document.getElementById("article-intro").style.display = "block";
+}
+function openArticleTwo() {
+    document.getElementById("article-work").style.display = "block";
+}
+function openArticleThree() {
+    document.getElementById("article-skills").style.display= "block";
+}
+function openArticleFour() {
+    document.getElementById("article-contact").style.display= "block";
+}
+  
+function closeArticleOne() {
+    document.getElementById("article-intro").style.display = "none";
+}
+function closeArticleTwo() {
+    document.getElementById("article-work").style.display = "none";
+}
+function closeArticleThree() {
+    document.getElementById("article-skills").style.display = "none";
+}
+function closeArticleFour() {
+    document.getElementById("article-contact").style.display = "none";
+}
